0530c4f19d9f27ce67ebd7cc6e3b52d96d9dbe9d
mattmoor <mattmoor@google.com>
2017-08-25 17:22:20 -0700

    Silence the go linter

--- docker/testdata/main.go
+++ docker/testdata/main.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package main tells the linter to shut up.
 package main
 
 import "fmt"
