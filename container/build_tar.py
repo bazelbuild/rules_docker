f5432b813e0a11491cf2bf83ff1a923706b36420
tejal29 <tejaldesai@google.com>
2017-12-21 11:34:06 -0800

    Generate a per package metadata file in /var/lib/dpkg/status.d directory for drydock (#239)
    * Add debian status file for dpgks
    
    Code review comments and Fix line length
    
    add unit tests
    
    code review comments plus docker targets
    
    add real example for deb
    
    Add tags to skip building target on darwin
    
    * Fix ci.json
    
    * Add tag to test target too
    
    *  Add keyword args
    
    * Remove actual deb from tests

--- container/build_tar.py
+++ container/build_tar.py
@@ -13,9 +13,11 @@
 # limitations under the License.
 """This tool build tar files from a list of inputs."""
 
+from contextlib import contextmanager
 import os
 import os.path
 import sys
+import re
 import tarfile
 import tempfile
 
@@ -78,6 +80,18 @@ class TarFile(object):
 
   class DebError(Exception):
     pass
+
+  PKG_NAME_RE = re.compile(r'Package:\s*(?P<pkg_name>\w+).*')
+  DPKG_STATUS_DIR = '/var/lib/dpkg/status.d'
+  PKG_METADATA_FILE = 'control'
+
+  @staticmethod
+  def parse_pkg_name(metadata, filename):
+    pkg_name_match = TarFile.PKG_NAME_RE.match(metadata)
+    if pkg_name_match:
+      return pkg_name_match.group('pkg_name')
+    else:
+      return os.path.basename(os.path.splitext(filename)[0])
 
   def __init__(self, output, directory, compression):
     self.directory = directory
@@ -178,6 +192,35 @@ class TarFile(object):
     symlink = os.path.normpath(symlink)
     self.tarfile.add_file(symlink, tarfile.SYMTYPE, link=destination)
 
+  @contextmanager
+  def write_temp_file(self, data, suffix='tar', mode='wb'):
+    (_, tmpfile) = tempfile.mkstemp(suffix=suffix)
+    try:
+      with open(tmpfile, mode='wb') as f:
+        f.write(data)
+      yield tmpfile
+    finally:
+      os.remove(tmpfile)
+
+  def add_pkg_metadata(self, metadata_tar, deb):
+    try:
+      with tarfile.open(metadata_tar) as tar:
+        # Metadata is expected to be in a file.
+        control_file_member = filter(lambda f: os.path.basename(f.name) == TarFile.PKG_METADATA_FILE, tar.getmembers())
+        if not control_file_member:
+           raise self.DebError(deb + ' does not Metadata File!')
+        control_file = tar.extractfile(control_file_member[0])
+        metadata = ''.join(control_file.readlines())
+        destination_file = os.path.join(TarFile.DPKG_STATUS_DIR,
+                                        TarFile.parse_pkg_name(metadata, deb))
+        with self.write_temp_file(data=metadata) as metadata_file:
+          self.add_file(metadata_file, destination_file)
+    except (KeyError, TypeError) as e:
+      raise self.DebError(deb + ' contains invalid Metadata! Exeception {0}'.format(e))
+    except Exception as e:
+      raise self.DebError('Unknown Exception {0}. Please report an issue at'
+                          ' github.com/bazelbuild/rules_docker.'.format(e))
+
   def add_deb(self, deb):
     """Extract a debian package in the output tar.
 
@@ -191,18 +234,30 @@ class TarFile(object):
     Raises:
       DebError: if the format of the deb archive is incorrect.
     """
+    pkg_data_found = False
+    pkg_metadata_found = False
     with archive.SimpleArFile(deb) as arfile:
       current = arfile.next()
-      while current and not current.filename.startswith('data.'):
+      while current:
+        parts = current.filename.split(".")
+        name = parts[0]
+        ext = '.'.join(parts[1:])
+        if name == 'data':
+          pkg_data_found = True
+          # Add pkg_data to image tar
+          with self.write_temp_file(suffix=ext, data=current.data) as tmpfile:
+            self.add_tar(tmpfile)
+        elif name == 'control':
+          pkg_metadata_found = True
+          # Add metadata file to image tar
+          with self.write_temp_file(suffix=ext, data=current.data) as tmpfile:
+            self.add_pkg_metadata(metadata_tar=tmpfile, deb=deb)
         current = arfile.next()
-      if not current:
-        raise self.DebError(deb + ' does not contains a data file!')
-      tmpfile = tempfile.mkstemp(suffix=os.path.splitext(current.filename)[-1])
-      with open(tmpfile[1], 'wb') as f:
-        f.write(current.data)
-      self.add_tar(tmpfile[1])
-      os.remove(tmpfile[1])
 
+    if not pkg_data_found:
+      raise self.DebError(deb + ' does not contains a data file!')
+    if not pkg_metadata_found:
+      raise self.DebError(deb + ' does not contains a control file!')
 
 def main(unused_argv):
   # Parse modes arguments
