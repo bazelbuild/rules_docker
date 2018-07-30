883bd3877d808e2279d7336ba1bca8fb5bcfdfba
hrfuller <henry@descarteslabs.com>
2018-07-30 20:03:45 +0000

    move lzma init out of except

--- container/build_tar.py
+++ container/build_tar.py
@@ -28,6 +28,7 @@ import warnings
 from tools.build_defs.pkg import archive
 from third_party.py import gflags
 
+lzma = None
 try:
   import lzma
 except ImportError:
@@ -38,7 +39,6 @@ except ImportError:
       "Could not find the lzma module. Building from XZ compressed tar files"
       " will fail."
     )
-    lzma = None
 
 gflags.DEFINE_string('output', None, 'The output file, mandatory')
 gflags.MarkFlagAsRequired('output')
