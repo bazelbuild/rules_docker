8eae94f4f9163ed8657e4b648f7dedbaf889191d
Nicolas Lopez <nlopezgi@gmail.com>
2018-11-09 09:29:32 -0500

    migrate first java_image tests from image_test.py to container_test (#576)
    * migrate first java_image tests from image_test.py to container_test
    
    * fix buildifier

--- container/image_test.py
+++ container/image_test.py
@@ -687,23 +687,6 @@ class ImageTest(unittest.TestCase):
         './app/com_google_guava_guava/jar/guava-18.0.jar',
       ])
 
-      self.assertConfigEqual(img, 'Entrypoint', [
-        '/usr/bin/java', '-cp',
-        ':'.join([
-          '/app/io_bazel_rules_docker/testdata/libjava_image_library.jar',
-          '/app/io_bazel_rules_docker/../com_google_guava_guava/jar/guava-18.0.jar',
-          '/app/io_bazel_rules_docker/testdata/java_image.binary.jar',
-          '/app/io_bazel_rules_docker/testdata/java_image.binary',
-          '/app/io_bazel_rules_docker/testdata/BUILD',
-        ]),
-        '-XX:MaxPermSize=128M',
-        '-Dbuild.location=testdata/BUILD',
-        'examples.images.Binary',
-        'arg0',
-        'arg1',
-        'testdata/BUILD'
-      ])
-
   def test_java_runfiles_image(self):
     with TestImage('java_runfiles_image') as img:
       # Check the application layer, which is on top.
@@ -829,24 +812,6 @@ class ImageTest(unittest.TestCase):
         '/app/testdata/py3_image.binary',
       ])
       self.assertConfigEqual(img, 'Cmd', [
-        'arg0',
-        'arg1',
-        'testdata/BUILD',
-      ])
-
-  def test_java_image_args(self):
-    with TestImage('java_image') as img:
-      self.assertConfigEqual(img, 'Entrypoint', [
-        '/usr/bin/java',
-        '-cp',
-        '/app/io_bazel_rules_docker/testdata/libjava_image_library.jar:'
-        +'/app/io_bazel_rules_docker/../com_google_guava_guava/jar/guava-18.0.jar:'
-        +'/app/io_bazel_rules_docker/testdata/java_image.binary.jar:'
-        +'/app/io_bazel_rules_docker/testdata/java_image.binary:'
-        +'/app/io_bazel_rules_docker/testdata/BUILD',
-        '-XX:MaxPermSize=128M',
-        '-Dbuild.location=testdata/BUILD',
-        'examples.images.Binary',
         'arg0',
         'arg1',
         'testdata/BUILD',
