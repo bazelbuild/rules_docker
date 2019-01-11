bazelbuild/rules_docker

commit 5eb0728594013d746959c4bd21aa4b0c3e3848d8

Add 'digest' field to PushInfo provider. (#591)
* Add 'digest' field to PushInfo provider. The field's value is a <File>
containing the to-be-published image digest.

* Add named output "%{name}.digest" to container_push.

* Add file_test for docker_push digest.

tests/docker/BUILD
@@ -207,3 +207,9 @@ container_push(
     tag = "test",
     tags = ["manual"],
 )
+
+file_test(
+    name = "test_push_digest_output",
+    content = "sha256:515b5206d4232e54ac4a0c2b45fb9fbe678ccc4f6c228e59024ac3ef61e08133",
+    file = ":push_test.digest",
+)

container/providers.bzl
@@ -48,6 +48,7 @@ PushInfo = provider(fields = [
     "tag",
     "stamp",
     "stamp_inputs",
+    "digest",
 ])
 
 # A provider containing information exposed by filter_layer rules

container/push-tag.sh.tpl
@@ -28,4 +28,4 @@ function guess_runfiles() {
 
 RUNFILES="${PYTHON_RUNFILES:-$(guess_runfiles)}"
 
-%{container_pusher} %{format} --name=%{tag} %{stamp} %{image} "$@"
+%{container_pusher} %{args} "$@"

container/push.bzl
@@ -33,88 +33,93 @@ def _get_runfile_path(ctx, f):
 
 def _impl(ctx):
     """Core implementation of container_push."""
-    stamp_inputs = []
-    if ctx.attr.stamp:
-        stamp_inputs = [ctx.info_file, ctx.version_file]
-
-    image = _get_layers(ctx, ctx.label.name, ctx.attr.image)
-
-    stamp_arg = " ".join(["--stamp-info-file=%s" % _get_runfile_path(ctx, f) for f in stamp_inputs])
+    pusher_args = []
+    digester_args = ctx.actions.args()
 
     # Leverage our efficient intermediate representation to push.
-    legacy_base_arg = ""
-    if image.get("legacy"):
+    image = _get_layers(ctx, ctx.label.name, ctx.attr.image)
+    blobsums = image.get("blobsum", [])
+    blobs = image.get("zipped_layer", [])
+    config = image["config"]
+    manifest = image["manifest"]
+    tarball = image.get("legacy")
+    image_files = blobs + blobsums
+
+    if tarball:
         print("Pushing an image based on a tarball can be very " +
               "expensive.  If the image is the output of a " +
               "docker_build, consider dropping the '.tar' extension. " +
               "If the image is checked in, consider using " +
               "docker_import instead.")
-        legacy_base_arg = "--tarball=%s" % _get_runfile_path(ctx, image["legacy"])
+        pusher_args += ["--tarball=%s" % _get_runfile_path(ctx, tarball)]
+        digester_args.add("--tarball", tarball)
+        image_files += [tarball]
+    if config:
+        pusher_args += ["--config=%s" % _get_runfile_path(ctx, config)]
+        digester_args.add("--config", config)
+        image_files += [config]
+    if manifest:
+        pusher_args += ["--manifest=%s" % _get_runfile_path(ctx, manifest)]
+        digester_args.add("--manifest", manifest)
+        image_files += [manifest]
+    for f in blobsums:
+        pusher_args += ["--digest=%s" % _get_runfile_path(ctx, f)]
+        digester_args.add("--digest", f)
+    for f in blobs:
+        pusher_args += ["--layer=%s" % _get_runfile_path(ctx, f)]
+        digester_args.add("--layer", f)
+    if ctx.attr.format == "OCI":
+        pusher_args += ["--oci"]
+        digester_args.add("--oci")
 
-    blobsums = image.get("blobsum", [])
-    digest_arg = " ".join(["--digest=%s" % _get_runfile_path(ctx, f) for f in blobsums])
-    blobs = image.get("zipped_layer", [])
-    layer_arg = " ".join(["--layer=%s" % _get_runfile_path(ctx, f) for f in blobs])
-    config_arg = "--config=%s" % _get_runfile_path(ctx, image["config"])
-    manifest_arg = "--manifest=%s" % _get_runfile_path(ctx, image["manifest"])
+    # create image digest
+    digester_args.add("--output-digest", ctx.outputs.digest)
+    ctx.actions.run(
+        inputs = image_files,
+        outputs = [ctx.outputs.digest],
+        executable = ctx.executable._digester,
+        arguments = [digester_args],
+        tools = ctx.attr._digester.default_runfiles.files,
+        mnemonic = "ContainerPushDigest",
+    )
 
+    # create pusher launcher
+    registry = ctx.expand_make_variables("registry", ctx.attr.registry, {})
+    repository = ctx.expand_make_variables("repository", ctx.attr.repository, {})
+    tag = ctx.expand_make_variables("tag", ctx.attr.tag, {})
+    stamp_inputs = [ctx.info_file, ctx.version_file] if ctx.attr.stamp else []
+    pusher_args += [
+        "--stamp-info-file=%s" % _get_runfile_path(ctx, f)
+        for f in stamp_inputs
+    ]
+    pusher_args += ["--name={registry}/{repository}:{tag}".format(
+        registry = registry,
+        repository = repository,
+        tag = tag,
+    )]
     ctx.template_action(
         template = ctx.file._tag_tpl,
         substitutions = {
-            "%{tag}": "{registry}/{repository}:{tag}".format(
-                registry = ctx.expand_make_variables(
-                    "registry",
-                    ctx.attr.registry,
-                    {},
-                ),
-                repository = ctx.expand_make_variables(
-                    "repository",
-                    ctx.attr.repository,
-                    {},
-                ),
-                tag = ctx.expand_make_variables(
-                    "tag",
-                    ctx.attr.tag,
-                    {},
-                ),
-            ),
-            "%{stamp}": stamp_arg,
-            "%{image}": "%s %s %s %s %s" % (
-                legacy_base_arg,
-                config_arg,
-                manifest_arg,
-                digest_arg,
-                layer_arg,
-            ),
-            "%{format}": "--oci" if ctx.attr.format == "OCI" else "",
+            "%{args}": " ".join(pusher_args),
             "%{container_pusher}": _get_runfile_path(ctx, ctx.executable._pusher),
         },
         output = ctx.outputs.executable,
         executable = True,
     )
+    runfiles = ctx.runfiles(files = [ctx.executable._pusher] + image_files + stamp_inputs)
+    runfiles = runfiles.merge(ctx.attr._pusher.default_runfiles)
 
-    runfiles = ctx.runfiles(
-        files = [
-                    ctx.executable._pusher,
-                    image["config"],
-                    image["manifest"],
-                ] + image.get("blobsum", []) + image.get("zipped_layer", []) +
-                stamp_inputs + ([image["legacy"]] if image.get("legacy") else []) +
-                list(ctx.attr._pusher.default_runfiles.files),
-    )
-
-    return struct(
-        providers = [
-            PushInfo(
-                registry = ctx.expand_make_variables("registry", ctx.attr.registry, {}),
-                repository = ctx.expand_make_variables("repository", ctx.attr.repository, {}),
-                tag = ctx.expand_make_variables("tag", ctx.attr.tag, {}),
-                stamp = ctx.attr.stamp,
-                stamp_inputs = stamp_inputs,
-            ),
-            DefaultInfo(executable = ctx.outputs.executable, runfiles = runfiles),
-        ],
-    )
+    return [
+        DefaultInfo(executable = ctx.outputs.executable, runfiles = runfiles),
+        PushInfo(
+            registry = registry,
+            repository = repository,
+            tag = tag,
+            stamp = ctx.attr.stamp,
+            stamp_inputs = stamp_inputs,
+            digest = ctx.outputs.digest,
+        ),
+    ]
 
 container_push = rule(
     attrs = dict({
@@ -142,6 +147,11 @@ container_push = rule(
             executable = True,
             allow_files = True,
         ),
+        "_digester": attr.label(
+            default = "@containerregistry//:digester",
+            cfg = "host",
+            executable = True,
+        ),
         "stamp": attr.bool(
             default = False,
             mandatory = False,
@@ -149,6 +159,9 @@ container_push = rule(
     }.items() + _layer_tools.items()),
     executable = True,
     implementation = _impl,
+    outputs = {
+        "digest": "%{name}.digest",
+    },
 )
 
 """Pushes a container image.

commit 8fa0e2d96046b950460e08a70ff3d7c65c40142e

Fix container bundle pusher broken by #591
This fixes #609. Also added end to end test to verify bundle pusher and
regular pusher upload legal images.

WORKSPACE
@@ -94,6 +94,16 @@ container_pull(
     tag = "3.1",
 )
 
+container_pull(
+    name = "test_pull_files_in_layer_with_files_base",
+    registry = "localhost:5000",
+    # STAMP_VALUE is the expanded version of the {TEST_STAMP} bazel
+    # stamp variable generated by print-workspace-status.sh during the e2e
+    # tests.
+    repository = "docker/test/stamp_value",
+    tag="stamp_value",
+)
+
 # For testing, don't change the sha on these ones
 container_pull(
     name = "distroless_fixed_id",

contrib/push-all.bzl
@@ -64,15 +64,15 @@ def _impl(ctx):
         ctx.actions.expand_template(
             template = ctx.file._tag_tpl,
             substitutions = {
-                "%{stamp}": stamp_arg,
-                "%{tag}": ctx.expand_make_variables("tag", tag, {}),
-                "%{image}": "%s %s %s %s" % (
+                "%{args}": "%s --name=%s %s %s %s %s %s" % (
+                    "--oci" if ctx.attr.format == "OCI" else "",
+                    ctx.expand_make_variables("tag", tag, {}),
+                    stamp_arg,
                     legacy_base_arg,
                     config_arg,
                     digest_arg,
                     layer_arg,
                 ),
-                "%{format}": "--oci" if ctx.attr.format == "OCI" else "",
                 "%{container_pusher}": _get_runfile_path(ctx, ctx.executable._pusher),
             },
             output = out,

print-workspace-status.sh
@@ -1,0 +1,21 @@
+#!/usr/bin/env bash
+# Copyright 2017 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Stamp variable to be expanded by bazel from TEST_STAMP to stamp_value
+echo TEST_STAMP stamp_value

testing/e2e.sh
@@ -466,12 +466,31 @@ function test_container_pull_with_auth() {
 function test_container_push_with_stamp() {
   cd "${ROOT}"
   clear_docker
+  bazel_opts=" --workspace_status_command=${ROOT}/print-workspace-status.sh"
   cid=$(docker run --rm -d -p 5000:5000 --name registry registry:2)
-  bazel run tests/docker:push_stamped_test
+  echo "Pushing stamped image to local registry"
+  bazel run $bazel_opts tests/docker:push_stamped_test
+  echo "Pulling stamped image from local registry"
+  EXPECT_CONTAINS "$(docker pull localhost:5000/docker/test/stamp_value:stamp_value)" "Gouda"
+  docker stop -t 0 $cid
+}
+
+function test_container_push_all() {
+  cd "${ROOT}"
+  clear_docker
+  cid=$(docker run --rm -d -p 5000:5000 --name registry registry:2)
+  # Use bundle push to push three images to the local registry.
+  bazel run tests/contrib:test_docker_push_three_images_bundle
+  # Pull the three images we just pushed to ensure uploaded manifests
+  # are valid according to docker.
+  EXPECT_CONTAINS "$(docker pull localhost:5000/image0:latest)" "Downloaded newer image"
+  EXPECT_CONTAINS "$(docker pull localhost:5000/image1:latest)" "Downloaded newer image"
+  EXPECT_CONTAINS "$(docker pull localhost:5000/image2:latest)" "Downloaded newer image"
   docker stop -t 0 $cid
 }
 
 test_container_push_with_stamp
+test_container_push_all
 test_container_push_with_auth
 test_container_pull_with_auth
 test_top_level

tests/contrib/BUILD
@@ -154,3 +154,17 @@ docker_push(
     name = "test_docker_push_empty_bundle",
     bundle = ":create_empty_bundle",
 )
+
+container_bundle(
+    name = "three_images_bundle",
+    images = {
+        "localhost:5000/image0:latest": "@io_bazel_rules_docker//testdata:base_with_entrypoint",
+        "localhost:5000/image1:latest": "@io_bazel_rules_docker//testdata:link_with_files_base",
+        "localhost:5000/image2:latest": "@io_bazel_rules_docker//testdata:with_double_env",
+    },
+)
+
+docker_push(
+    name = "test_docker_push_three_images_bundle",
+    bundle = ":three_images_bundle",
+)

tests/docker/BUILD
@@ -252,10 +252,10 @@ container_push(
 container_push(
     name = "push_stamped_test",
     format = "Docker",
-    image = ":set_cmd",
+    image = "@io_bazel_rules_docker//testdata:files_in_layer_with_files_base",
     registry = "localhost:5000",
-    repository = "docker/test/{BUILD_USER}",
-    tag = "{BUILD_USER}",
+    repository = "docker/test/{TEST_STAMP}",
+    tag = "{TEST_STAMP}",
     tags = ["manual"],
 )
 