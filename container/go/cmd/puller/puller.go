847eed0889a1bb51ad5336953b68aa171aad29c7
Winnie Xu <winnie.xu97@gmail.com>
2019-06-07 15:41:13 -0400

    resolve docs consistency and import name clash

--- container/go/cmd/puller/puller.go
+++ container/go/cmd/puller/puller.go
@@ -20,20 +20,20 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
+	ospkg "os"
 )
 
 var (
 	imgName         = flag.String("name", "", "The name location including repo and digest/tag of the docker image to pull and save. Supports fully-qualified tag or digest references.")
 	directory       = flag.String("directory", "", "Where to save the images files.")
-	clientConfigDir = flag.String("client-config-dir", "", "Specifies where the custom docker config.json is located. Overiddes the value from DOCKER_CONFIG.")
+	clientConfigDir = flag.String("client-config-dir", "", "The path to the directory where the client configuration files are located. Overiddes the value from DOCKER_CONFIG.")
 	cachePath       = flag.String("cache", "", "Image's files cache directory.")
 	arch            = flag.String("architecture", "", "Image platform's CPU architecture.")
-	os1             = flag.String("os", "linux", "The image's OS, if referring to a multi-platform manifest list. Default linux.")
-	osVers          = flag.String("os-version", "", "The image's os version to pull if referring to a multi-platform manifest list.")
-	osFeat          = flag.String("os-features", "", "The image's os features when pulling a multi-platform manifest list.")
-	variant         = flag.String("variant", "", "The desired CPU variant when image refers to a multi-platform manifest list.")
-	features        = flag.String("features", "", "The desired platform features when image refers to a multi-platform manifest list.")
+	os              = flag.String("os", "", "Image's operating system, if referring to a multi-platform manifest list. Default linux.")
+	osVersion       = flag.String("os-version", "", "Image's operating system version, if referring to a multi-platform manifest list.")
+	osFeature       = flag.String("os-features", "", "Image's operating system features, if referring to a multi-platform manifest list.")
+	variant         = flag.String("variant", "", "Image's CPU variant, if referring to a multi-platform manifest list.")
+	features        = flag.String("features", "", "Image's CPU features, if referring to a multi-platform manifest list.")
 )
 
 func main() {
@@ -50,7 +50,7 @@ func main() {
 	// If the user provided a client config directory, instruct the keychain resolver
 	// to use it to look for the docker client config
 	if *clientConfigDir != "" {
-		os.Setenv("DOCKER_CONFIG", *clientConfigDir)
+		ospkg.Setenv("DOCKER_CONFIG", *clientConfigDir)
 	}
 
 	log.Printf("Successfully pulled image %q into %q", *imgName, *directory)
