548220c52beb484a0afc32dba5403e5972d2b676
Winnie Xu <winnie.xu97@gmail.com>
2019-06-13 15:50:43 -0400

    new puller binary

--- container/go/cmd/puller/puller.go
+++ container/go/cmd/puller/puller.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 // This binary pulls images from a Docker Registry using the go-containerregistry as backend.
 // Unlike regular docker pull, the format this package uses is proprietary.
 
