c52621b79f11801146bed0e0b40dbfb42aab7e74
Winnie Xu <winnie.xu97@gmail.com>
2019-06-13 13:53:04 -0400

    update puller binary to write images in OCI format

--- container/go/cmd/puller/puller.go
+++ container/go/cmd/puller/puller.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The Bazel Authors. All rights reserved.
+/// Copyright 2015 The Bazel Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,17 +19,16 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	ospkg "os"
 
+	"github.com/bazelbuild/rules_docker/container/go/pkg/oci"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/cache"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
-	"github.com/google/go-containerregistry/pkg/v1/tarball"
 )
 
 var (
@@ -55,46 +54,26 @@ const iWasADigestTag = "i-was-a-digest"
 // Pull the image with given <imgName> to destination <dstPath> with optional
 // cache files and required platform specifications.
 func pull(imgName, dstPath, cachePath string, platform v1.Platform) {
-	// Get a digest/tag based on the name
+	// Get a digest/tag based on the name.
 	ref, err := name.ParseReference(imgName)
 	if err != nil {
 		log.Fatalf("parsing tag %q: %v", imgName, err)
 	}
 	log.Printf("Pulling %v", ref)
 
-	// Fetch the image with desired cache files and platform specs
-	i, err := remote.Image(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain), remote.WithPlatform(platform))
+	// Fetch the image with desired cache files and platform specs.
+	img, err := remote.Image(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain), remote.WithPlatform(platform))
 	if err != nil {
 		log.Fatalf("reading image %q: %v", ref, err)
 	}
 	if cachePath != "" {
-		i = cache.Image(i, cache.NewFilesystemCache(cachePath))
+		img = cache.Image(img, cache.NewFilesystemCache(cachePath))
 	}
 
-	// WriteToFile wants a tag to write to the tarball, but we might have
-	// been given a digest.
-	// If the original ref was a tag, use that. Otherwise, if it was a
-	// digest, tag the image with :i-was-a-digest instead.
-	tag, ok := ref.(name.Tag)
-	if !ok {
-		d, ok := ref.(name.Digest)
-		if !ok {
-			log.Fatal("ref wasn't a tag or digest")
-		}
-		s := fmt.Sprintf("%s:%s", d.Repository.Name(), iWasADigestTag)
-		tag, err = name.NewTag(s)
-		if err != nil {
-			log.Fatalf("parsing digest as tag (%s): %v", s, err)
-		}
-	}
-
-	// file to write to
-	var dstFile string
-	// TODO(xwinxu): figure out the output tarball mechanism, currently hard coded to "img"
-	dstFile = dstPath + "/" + "img"
-
-	if err := tarball.WriteToFile(dstFile, tag, i); err != nil {
-		log.Fatalf("writing image %q: %v", dstFile, err)
+	// // Image file to write to disk.
+	if err := oci.Write(img, dstPath); err != nil {
+		// if err := oci.Write(img, path); err != nil {
+		log.Fatalf("failed to write image to %q: %v", dstPath, err)
 	}
 }
 
@@ -114,8 +93,6 @@ func main() {
 	if *clientConfigDir != "" {
 		ospkg.Setenv("DOCKER_CONFIG", *clientConfigDir)
 	}
-
-	// log.Fatalf("here")
 
 	// Create a Platform struct with arguments
 	platform := v1.Platform{
