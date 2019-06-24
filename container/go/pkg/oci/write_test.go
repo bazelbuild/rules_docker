c505135aee152dc57c70e61865fa64623ab0fdff
Winnie Xu <winnie.xu97@gmail.com>
2019-06-24 17:04:27 -0400

    refactor repetitive sections into single helper function

--- container/go/pkg/oci/write_test.go
+++ container/go/pkg/oci/write_test.go
@@ -18,9 +18,9 @@ import (
 )
 
 var (
-	blob     = "/blobs/"
-	index    = "/index.json"
-	ocif     = "/oci-layout"
+	blob     = "blobs/"
+	index    = "index.json"
+	ocif     = "oci-layout"
 	testPath = "testdata/test_write1"
 )
 
@@ -100,69 +100,30 @@ func assertOCIFormat(tmp string, img v1.Image, content map[string]v1.Hash, shaPt
 	//     oci-layout, index.json, blobs/
 	// Under blobs/, for each image:
 	// 	   one file for each layer, config blob, and manifest blob
-	blobs := tmp + blob
-	blob, err := os.Stat(blobs)
-	if err != nil {
-		return fmt.Errorf("could not obtain stat; blob doesn't exist: %v", err)
-	}
-	if os.IsNotExist(err) {
-		return fmt.Errorf("wrong OCI output format; got: %v, missing: %s", err, blob)
-	}
-	if !blob.IsDir() {
-		return fmt.Errorf("failed to write blobs/ directory to %s", tmp)
-	}
-	f, err := os.Open(blobs)
-	if err != nil {
-		return fmt.Errorf("failed to open directory blobs: %v", err)
-	}
-	defer f.Close()
-	_, err = f.Readdirnames(1)
-	if err == io.EOF {
-		return fmt.Errorf("blobs directory is empty: %v", err)
-	}
-	if err = compareSHA(path.Join(blobs, shaPth), "blobs file at "+shaPth, content["blobs/sha256/9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c"].Hex); err != nil {
-		return fmt.Errorf("sha256 hex code did not match %v", err)
+	blobs := path.Join(tmp, blob)
+	file := path.Join(blobs, "sha256", "9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c")
+	if err := validateDir(blobs, content["blobs/sha256/9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c"].Hex, tmp, file); err != nil {
+		return fmt.Errorf("failed to validate blobs/ directory: %v", err)
 	}
 
 	// validate the index.json file
-	idx, err := os.Stat(path.Join(tmp, index))
-	if err != nil {
-		return fmt.Errorf("could not obtain stat: %v", err)
-	}
-
-	if os.IsNotExist(err) {
-		return fmt.Errorf("wrong OCI output format; got: %v, missing: %s", err, index)
-	}
-	if idx.Size() == 0 {
-		return fmt.Errorf("error occured: empty index.json, size: %d", idx.Size())
-	}
-	if err = compareSHA(path.Join(tmp, index), "oci-layout", content["index.json"].Hex); err != nil {
-		return fmt.Errorf("sha256 hex code did not match, %v", err)
+	if err := validateFile(path.Join(tmp, index), index, content["index.json"].Hex, tmp); err != nil {
+		return fmt.Errorf("failed to validate index.json file: %v", err)
 	}
 
 	// validate the oci-layout file
-	oci, err := os.Stat(tmp + ocif)
-	if err != nil {
-		return fmt.Errorf("could not obtain stat: %v", err)
-	}
-	if os.IsNotExist(err) {
-		return fmt.Errorf("wrong OCI output format; got: %v, missing: %s", err, index)
-	}
-	if oci.Size() == 0 {
-		return fmt.Errorf("error occured: empty oci-layout, size: %d", oci.Size())
-	}
-	if err = compareSHA(path.Join(tmp, ocif), "oci-layout", content["oci-layout"].Hex); err != nil {
-		return fmt.Errorf("sha256 hex code did not match, %v", err)
+	if err := validateFile(path.Join(tmp, ocif), ocif, content["oci-layout"].Hex, tmp); err != nil {
+		return fmt.Errorf("failed to validate index.json file: %v", err)
 	}
 
 	return nil
 }
 
 // compares the SHA256 value of src file to example
-func compareSHA(src, tag, want string) error {
+func compareSHA(src, want string) error {
 	f, err := os.Open(src)
 	if err != nil {
-		return fmt.Errorf("failed to open %s file", tag)
+		return fmt.Errorf("failed to open file: %v", err)
 	}
 	defer f.Close()
 
@@ -173,6 +134,54 @@ func compareSHA(src, tag, want string) error {
 	got := hex.EncodeToString(h.Sum(nil))
 	if strings.Compare(got, want) != 0 {
 		return fmt.Errorf("sha256 hex code did not match, wanted: %s got: %s", want, got)
+	}
+
+	return nil
+}
+
+// validates a directory being tested for by checking directory is not empty or corrupt
+func validateDir(src, want, tmp, file string) error {
+	dir, err := os.Stat(src)
+	if err != nil {
+		return fmt.Errorf("could not obtain stat; dir doesn't exist: %v", err)
+	}
+	if os.IsNotExist(err) {
+		return fmt.Errorf("wrong OCI output format; got: %v, missing: %s", err, dir)
+	}
+	if !dir.IsDir() {
+		return fmt.Errorf("failed to write directory to %s", tmp)
+	}
+	f, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("failed to open directory: %v", err)
+	}
+	defer f.Close()
+	_, err = f.Readdirnames(1)
+	if err == io.EOF {
+		return fmt.Errorf("directory %s is empty: %v", src, err)
+	}
+	if err = compareSHA(file, want); err != nil {
+		return fmt.Errorf("sha256 hex code did not match: %v", err)
+	}
+
+	return nil
+}
+
+// validates a file being tested for by checking file is not empty or corrupt
+func validateFile(src, file, want, tmp string) error {
+	f, err := os.Stat(src)
+	if err != nil {
+		return fmt.Errorf("could not obtain stat: %v", err)
+	}
+
+	if os.IsNotExist(err) {
+		return fmt.Errorf("wrong OCI output format; got: %v, missing: %s", err, file)
+	}
+	if f.Size() == 0 {
+		return fmt.Errorf("error occured: empty %s, size: %d", file, f.Size())
+	}
+	if err = compareSHA(path.Join(tmp, file), want); err != nil {
+		return fmt.Errorf("sha256 hex code did not match, %v", err)
 	}
 
 	return nil
