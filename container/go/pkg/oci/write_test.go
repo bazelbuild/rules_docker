8c2b84ab6ed2e05b5336759c1590df8bc4e86338
Winnie Xu <winnie.xu97@gmail.com>
2019-06-24 15:50:17 -0400

    extract sha256 check sum helper function

--- container/go/pkg/oci/write_test.go
+++ container/go/pkg/oci/write_test.go
@@ -20,7 +20,7 @@ import (
 var (
 	blob     = "/blobs/"
 	index    = "/index.json"
-	oci_     = "/oci-layout"
+	ocif     = "/oci-layout"
 	testPath = "testdata/test_write1"
 )
 
@@ -29,15 +29,18 @@ var testCases = []struct {
 	name string
 	// dataPath is where the test data is found
 	dataPath string
+	// the sha code for the manifest
+	shaPath string
 	// content is the oci layout content where the key is the file path and value is sha256 sum
 	content map[string]v1.Hash
 }{
 	{
 		"alpine_linux",
 		"testdata/test_write1/small_linux.tar",
+		"sha256/9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c",
 		map[string]v1.Hash{
 			"blobs/sha256/9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c": v1.Hash{Algorithm: "sha256", Hex: "9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c"},
-			"index.json": v1.Hash{Algorithm: "sha256", Hex: "603b14cfdfdd459fecc34d3c320bdfbe4bf81057e2e5090159102e4f01c70cb7"},
+			"index.json": v1.Hash{Algorithm: "sha256", Hex: "ce6ed2e6817add8a5a0e94f4c2ac00360c7ca2cc98c4972b86530f14de622508"},
 			"oci-layout": v1.Hash{Algorithm: "sha256", Hex: "b66dbb27a73334db6ac9c030475837bd7f4472d835c72b2360534b203edce6cb"},
 		},
 	},
@@ -63,7 +66,7 @@ func TestWrite(t *testing.T) {
 				t.Errorf("image written is not a valid index: %v", err)
 			}
 
-			if err = assertOCIFormat(tmp, img, wt.content); err != nil {
+			if err = assertOCIFormat(tmp, img, wt.content, wt.shaPath); err != nil {
 				t.Errorf("image is not in oci format: %v", err)
 			}
 		})
@@ -87,12 +90,12 @@ func assertImageIndexValid(tmp string, img v1.Image) error {
 	return nil
 }
 
-func assertOCIFormat(tmp string, img v1.Image, content map[string]v1.Hash) error {
+func assertOCIFormat(tmp string, img v1.Image, content map[string]v1.Hash, shaPth string) error {
 	if err := Write(img, "/usr/local/google/home/xwinxu/Desktop/temp/temp"); err != nil {
 		return fmt.Errorf("error writing image to temp path %s", tmp)
 	}
 
-	// check that the contents were written in the following format:
+	// validate that the contents of blobs/ were written in the following format:
 	// Top level:
 	//     oci-layout, index.json, blobs/
 	// Under blobs/, for each image:
@@ -105,7 +108,6 @@ func assertOCIFormat(tmp string, img v1.Image, content map[string]v1.Hash) error
 	if os.IsNotExist(err) {
 		return fmt.Errorf("wrong OCI output format; got: %v, missing: %s", err, blob)
 	}
-
 	if !blob.IsDir() {
 		return fmt.Errorf("failed to write blobs/ directory to %s", tmp)
 	}
@@ -118,32 +120,11 @@ func assertOCIFormat(tmp string, img v1.Image, content map[string]v1.Hash) error
 	if err == io.EOF {
 		return fmt.Errorf("blobs directory is empty: %v", err)
 	}
-	// Check if blobs file specified hashes to the same one in the test cases
-	bf, err := os.Open(blobs + "sha256/9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c")
-	if err != nil {
-		return fmt.Errorf("failed to open blobs file 9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c")
+	if err = compareSHA(path.Join(blobs, shaPth), "blobs file at "+shaPth, content["blobs/sha256/9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c"].Hex); err != nil {
+		return fmt.Errorf("sha256 hex code did not match %v", err)
 	}
-	h := sha256.New()
-	// print(h.Sum(nil))
-	// print("hi")
-	if _, err := io.Copy(h, bf); err != nil {
-		return fmt.Errorf("sha256 hashing failed: %v", err)
-	}
-	// hex := string(h.Sum(nil))
-	// hexe, _, err := v1.SHA256(bytes.NewReader(h.Sum(nil)))
-	// if err != nil {
-	// 	return fmt.Errorf("sha256 hashing failure: %v", err)
-	// }
-	// _ := hexe.Hex
-	got := hex.EncodeToString(h.Sum(nil))
-	want := content["blobs/sha256/9a96f3888ebad00d46bca04ccb591e70d091624835998668af551f48512d9b5c"].Hex
-	if got != want {
-		return fmt.Errorf("sha256 hex code does not match, wanted: %s got: %s, file incorrect: %v", want, got, err)
-	}
-	h.Reset()
-	// print(h.Sum(nil))
-	// print("here")
 
+	// validate the index.json file
 	idx, err := os.Stat(path.Join(tmp, index))
 	if err != nil {
 		return fmt.Errorf("could not obtain stat: %v", err)
@@ -155,27 +136,12 @@ func assertOCIFormat(tmp string, img v1.Image, content map[string]v1.Hash) error
 	if idx.Size() == 0 {
 		return fmt.Errorf("error occured: empty index.json, size: %d", idx.Size())
 	}
-	// Check if hash of index.json written matches what is expected
-	// h = sha256.New()
-	// idxf, err := os.Open(tmp + index)
-	// if err != nil {
-	// 	return fmt.Errorf("failed to open index.json file")
-	// }
-	// defer idxf.Close()
-	// if _, err := io.Copy(h, idxf); err != nil {
-	// 	return fmt.Errorf("sha256 hashing failed: %v", err)
-	// }
-	// // print(h.Sum(nil))
-	// got = hex.EncodeToString(h.Sum(nil))
-	// // fmt.Printf("%x", h.Sum(nil))
-	// // fmt.Printf("ahen")
-	// // fmt.Printf("%s", hex.EncodeToString(h.Sum(nil)))
-	// want = content["index.json"].Hex
-	// if strings.Compare(got, want) != 0 {
-	// 	return fmt.Errorf("sha256 hex code does not match for index.json, wanted: %s got: %s, file incorrect: %v", want, got, err)
-	// }
+	if err = compareSHA(path.Join(tmp, index), "oci-layout", content["index.json"].Hex); err != nil {
+		return fmt.Errorf("sha256 hex code did not match, %v", err)
+	}
 
-	oci, err := os.Stat(tmp + oci_)
+	// validate the oci-layout file
+	oci, err := os.Stat(tmp + ocif)
 	if err != nil {
 		return fmt.Errorf("could not obtain stat: %v", err)
 	}
@@ -185,25 +151,28 @@ func assertOCIFormat(tmp string, img v1.Image, content map[string]v1.Hash) error
 	if oci.Size() == 0 {
 		return fmt.Errorf("error occured: empty oci-layout, size: %d", oci.Size())
 	}
-	// Check if hash of oci-layout == the one in dict
-	h = sha256.New()
-	print(path.Join(tmp, oci_))
-	ocif, err := os.Open(tmp + oci_)
-	if err != nil {
-		return fmt.Errorf("failed to open oci-layout file")
+	if err = compareSHA(path.Join(tmp, ocif), "oci-layout", content["oci-layout"].Hex); err != nil {
+		return fmt.Errorf("sha256 hex code did not match, %v", err)
 	}
-	defer ocif.Close()
-	if _, err := io.Copy(h, ocif); err != nil {
+
+	return nil
+}
+
+// compares the SHA256 value of src file to example
+func compareSHA(src, tag, want string) error {
+	f, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("failed to open %s file", tag)
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
 		return fmt.Errorf("sha256 hashing failed: %v", err)
 	}
-	// print(h.Sum(nil))
-	got = hex.EncodeToString(h.Sum(nil))
-	// fmt.Printf("%x", h.Sum(nil))
-	// fmt.Printf("ahen")
-	// fmt.Printf("%s", hex.EncodeToString(h.Sum(nil)))
-	want = content["oci-layout"].Hex
+	got := hex.EncodeToString(h.Sum(nil))
 	if strings.Compare(got, want) != 0 {
-		return fmt.Errorf("sha256 hex code does not match for oci-layout, wanted: %s got: %s, file incorrect: %v", want, got, err)
+		return fmt.Errorf("sha256 hex code did not match, wanted: %s got: %s", want, got)
 	}
 
 	return nil
