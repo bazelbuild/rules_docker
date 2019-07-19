960db6c5c7b71c26fc3243ee2498ca480245554f
Nicolas Lopez <ngiraldo@google.com>
2019-07-19 09:54:55 -0400

    clean py imports

--- docker/util/to_json.py
+++ docker/util/to_json.py
@@ -14,5 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys,json;
+import json
+import sys
+
 print json.dumps(sys.stdin.read().strip().split())
