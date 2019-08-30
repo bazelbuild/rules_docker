14279cd458f1103620d634fa35ca87e6e058d478
Suvanjan Mukherjee <suvanjan@google.com>
2019-08-30 14:41:08 -0400

    Address review comments

--- container/go/pkg/utils/utils.go
+++ container/go/pkg/utils/utils.go
@@ -1,3 +1,17 @@
+// Copyright 2015 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//////////////////////////////////////////////////////////////////////
 package utils
 
 import (
@@ -32,6 +46,8 @@ func (f *ArrayStringFlags) Set(value string) error {
 }
 
 // ReadImage returns a v1.Image after reading an image in OCI or Docker format.
+// Either *only* the image tarball must be specified or the image config along
+// with paths to the compressed layer tarballs.
 func ReadImage(imgConfig, imgTarball string, layersPath []string) (v1.Image, error) {
 	if imgTarball != "" && filepath.Ext(imgTarball) != ".tar" {
 		return nil, errors.Errorf("got invalid image tarball file, got %s, want path to file with extension .tar", imgTarball)
