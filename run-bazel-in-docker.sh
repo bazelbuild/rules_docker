8e4e4d7b31047fe253ca2260ccc8ba097d282afc
Sebastian Nemeth <sebastian@cleric.sh>
2021-05-20 06:57:40 +0300

    add documentation for run-bazel-in-docker.sh

--- run-bazel-in-docker.sh
+++ run-bazel-in-docker.sh
@@ -1,6 +1,10 @@
-mkdir -p /tmp/build_output/ 
+# Runs bazel in a docker container, with this repository's workspace mounted in its file system.
+# This is useful because rules_docker assumes the host environment is linux, and tests will fail on other environments.
+# Running bazel in docker is slower, but allows for the build and tests to execute on non-linux environments.
+
+mkdir -p /tmp/build_output/
 # -e USER="$(id -u)" \
-  # -u="$(id -u)" \
+# -u="$(id -u)" \
 docker run \
   -v "$PWD":/workspace \
   -v /tmp/build_output:/tmp/build_output \
