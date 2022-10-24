6c70c93524217995325d9585baca98d458bfc9b6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 10:12:19 +0000

    Adding tarfile member sanitization to extractall()

--- docker/util/config_stripper.py
+++ docker/util/config_stripper.py
@@ -53,7 +53,26 @@ def strip_tar(input, output):
 
     tempdir = tempfile.mkdtemp()
     with tarfile.open(name=input, mode='r') as it:
-        it.extractall(tempdir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(it, tempdir)
 
     mf_path = os.path.join(tempdir, 'manifest.json')
     with open(mf_path, 'r') as mf:
